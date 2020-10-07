8016e86bf9e884b18ee7ef0a3750b2b8cef1e297
lucnicolas <l.nicolas@intech-sud.fr>
2020-10-07 11:23:02 +0200

    ✨ Implementing routing + creating navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 
 import {AppComponent} from './app.component';
 import {SeriesComponent} from './components/series/series.component';
 import {SeasonsComponent} from './components/seasons/seasons.component';
 import {EpisodesComponent} from './components/episodes/episodes.component';
 
+const routes: Routes = [
+  { path: 'episodes', component: EpisodesComponent}
+]; // sets up routes constant where you define your routes
+
+// configures NgModule imports and exports
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +21,9 @@ import {EpisodesComponent} from './components/episodes/episodes.component';
   ],
   imports: [
     BrowserModule,
+    [RouterModule.forRoot(routes)]
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
