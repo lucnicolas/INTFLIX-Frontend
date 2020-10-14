00feea9fc1a37a752640b91d4b356095d68c6259
lucnicolas <l.nicolas@intech-sud.fr>
2020-10-14 08:52:31 +0200

    ✨ Adding routes to pages Adding routes to EpisodesComponent and creating a PageNotFound

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,13 @@ import {AppComponent} from './app.component';
 import {SeriesComponent} from './components/series/series.component';
 import {SeasonsComponent} from './components/seasons/seasons.component';
 import {EpisodesComponent} from './components/episodes/episodes.component';
+import { PageNotFoundComponent } from './components/PageNotFound/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'episodes', component: EpisodesComponent}
+  { path: 'episodes', component: EpisodesComponent},
+  { path: 'episodes/:id', component: EpisodesComponent},
+  { path: '',   redirectTo: '/episodes', pathMatch: 'full' }, // redirect to `episodes`
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ]; // sets up routes constant where you define your routes
 
 // configures NgModule imports and exports
@@ -17,7 +21,8 @@ const routes: Routes = [
     AppComponent,
     SeriesComponent,
     SeasonsComponent,
-    EpisodesComponent
+    EpisodesComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/episodes/episodes.component.ts
+++ src/app/components/episodes/episodes.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Episode} from '../../models/episode/episode';
-import {SeasonsComponent} from '../seasons/seasons.component';
 import {Season} from '../../models/season/season';
-import {Series} from '../../models/series/series';
 import {EpisodesService} from '../../controllers/episodes/episodes.service';
 
 @Component({
